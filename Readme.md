01ce961734bfaa47e6e810fdd40a1bd29b3a9c03
AdelKudryashev <adek-p@yandex.ru>
2022-12-19 14:00:42 +0300

    First version

---
+++ Readme.md
@@ -0,0 +1,1 @@
+On some vps, when using the Outline installation script, it gives a Docker installation error. This file contains commands to pre-install Docker. 
---
+++ startvpn.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt update
+apt install docker-ce -y
+bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)"
